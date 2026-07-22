d0b26bb58c68d5e1c4266c5f8908b02da79b8059
NotSoGreeeen <NotSoGreeeen@gmail.com>
2026-07-22 16:58:15 -0700

    Added duck toss code
    There was a merge conflict, so code may be borked? Good luck :)

--- gmtk-2026/Assets/Scripts/DuckTossDuck.cs
+++ gmtk-2026/Assets/Scripts/DuckTossDuck.cs
@@ -2,16 +2,58 @@ using UnityEngine;
 
 public class DuckTossDuck : MonoBehaviour
 {
+    [SerializeField]
+    public float speed;
+
+    private static float duckHeight;
+    private bool movingLeft = false;
+    [SerializeField]
+    private bool fed = false;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        duckHeight = transform.position.y;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (!fed)
+        {
+            // Bob in water
+            float y = duckHeight + Mathf.Sin(Time.time * 2f) * 0.1f;
+            transform.position = new Vector3(transform.position.x, y, transform.position.z);
+
+            if (transform.position.x < -7.5f)
+            {
+                movingLeft = false;
+                transform.localScale = new Vector3(1f, 1f, 1f);
+            }
+            else if (transform.position.x > 5f)
+            {
+                movingLeft = true;
+                transform.localScale = new Vector3(-1f, 1f, 1f);
+            }
+            
+            transform.position += new Vector3((movingLeft ? -1f : 1f) * Time.deltaTime * speed, 0, 0);
+        } else {
+            // Fly away
+            transform.position += new Vector3(0, 1f * Time.deltaTime * speed, 0);
+
+            if (transform.position.y > 10f)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Grape"))
+        {
+            fed = true;
+            Destroy(other.gameObject);
+        }
     }
 }
