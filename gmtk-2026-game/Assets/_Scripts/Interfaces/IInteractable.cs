3f58a9478fba94b0289433786f97c6bcf0abfe5f
Matthew Dinh <matthewhdinhhmk@gmail.com>
2026-07-23 22:01:54 -0700

    added lever interactability to new lever prefab

--- gmtk-2026-game/Assets/_Scripts/Interfaces/IInteractable.cs
+++ gmtk-2026-game/Assets/_Scripts/Interfaces/IInteractable.cs
@@ -2,8 +2,20 @@ using UnityEngine;
 
 public interface IInteractable
 {
+    /// <summary>
+    /// Called on the first frame the player starts interacting with this object.
+    /// </summary>
+    /// <param name="data"></param>
     void OnInteractStart(InteractionData data);
+    /// <summary>
+    /// Called every frame while the player is interacting with this object.
+    /// </summary>
+    /// <param name="data"></param>
     void OnInteractDrag(InteractionData data);
+    /// <summary>
+    /// Called on the first frame the player stops interacting with this object.
+    /// </summary>
+    /// <param name="data"></param>
     void OnInteractEnd(InteractionData data);
 }
 
