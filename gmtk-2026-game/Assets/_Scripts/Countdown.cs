fa2dd6c03b3c8eebfc22f210b37ed3311cfebb84
Matthew Dinh <matthewhdinhhmk@gmail.com>
2026-07-23 17:28:08 -0700

    add countdown script

---
+++ gmtk-2026-game/Assets/_Scripts/Countdown.cs
@@ -0,0 +1,61 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Countdown : MonoBehaviour
+{
+    [SerializeField] private float countdownTime = 10f;
+    [SerializeField] private float currentCountdownTime = 10f;
+    [SerializeField] private float countdownSpeed = 1f;
+    [SerializeField] private TextMeshProUGUI countdownText;
+    public UnityEvent onCountdownFinished;
+    public float CountdownTime => countdownTime;
+    public float CurrentCountdownTime => currentCountdownTime;
+    public float CountdownSpeed => countdownSpeed;
+
+    public void RestartCountdown()
+    {
+        currentCountdownTime = countdownTime;
+        UpdateCountdownText();
+    }
+    
+    public void SetCountdownTime(float time)
+    {
+        countdownTime = time;
+        currentCountdownTime = time;
+        UpdateCountdownText();
+    }
+
+    public void SetCountdownRunningSpeed(float speed)
+    {
+        countdownSpeed = speed;
+    }
+
+    private void UpdateCountdownText()
+    {
+        if (countdownText != null)
+        {
+            countdownText.text = currentCountdownTime.ToString("F1");
+        }
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        UpdateCountdownText();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentCountdownTime -= countdownSpeed * Time.deltaTime;
+        UpdateCountdownText();
+        if (currentCountdownTime <= 0)
+        {
+            currentCountdownTime = 0;
+            UpdateCountdownText();
+            onCountdownFinished?.Invoke();
+            // Countdown has reached zero, you can trigger an event or perform any action here
+        }
+    }
+}
