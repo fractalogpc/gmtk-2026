8fe228eb0bca7c0426c751fea9729df97c336df3
Elliot <93895160+2Elliot@users.noreply.github.com>
2026-07-25 01:07:45 -0700

    Radio visuals

--- gmtk-2026-game/Assets/_Scripts/DecoderController.cs
+++ gmtk-2026-game/Assets/_Scripts/DecoderController.cs
@@ -2,15 +2,95 @@ using UnityEngine;
 
 public class DecoderController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public DragDial leftDial;
+    public DragDial rightDial;
+
+    public GameObject verticalBar;
+    public GameObject horizontalBar;
+
+    [Tooltip("Local units the horizontal bar shifts vertically per degree of right-dial rotation.")]
+    [SerializeField] private float horizontalBarSensitivity = 0.005f;
+    [Tooltip("Local units the vertical bar shifts horizontally per degree of left-dial rotation.")]
+    [SerializeField] private float verticalBarSensitivity = 0.005f;
+
+    [Tooltip("Min/Max offset (local units) the vertical bar can shift along X from its start position.")]
+    [SerializeField] private Vector2 verticalBarOffsetClamp = new Vector2(-0.5f, 0.5f);
+    [Tooltip("Min/Max offset (local units) the horizontal bar can shift along Y from its start position.")]
+    [SerializeField] private Vector2 horizontalBarOffsetClamp = new Vector2(-0.5f, 0.5f);
+
+    private Vector3 verticalBarInitialWorldPos;
+    private Vector3 horizontalBarInitialWorldPos;
+
+    private float verticalBarOffset;
+    private float horizontalBarOffset;
+    private float previousLeftDialAngle;
+    private float previousRightDialAngle;
+
+    private void Awake()
     {
-        
+        if (verticalBar != null) verticalBarInitialWorldPos = verticalBar.transform.position;
+        if (horizontalBar != null) horizontalBarInitialWorldPos = horizontalBar.transform.position;
+        if (leftDial != null) previousLeftDialAngle = leftDial.Angle;
+        if (rightDial != null) previousRightDialAngle = rightDial.Angle;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        UpdateRadioVisuals();
     }
+
+    private void UpdateRadioVisuals()
+    {
+        if (verticalBar != null && leftDial != null)
+        {
+            float delta = leftDial.Angle - previousLeftDialAngle;
+            previousLeftDialAngle = leftDial.Angle;
+            verticalBarOffset = Mathf.Clamp(verticalBarOffset - delta * verticalBarSensitivity, verticalBarOffsetClamp.x, verticalBarOffsetClamp.y);
+            verticalBar.transform.position = verticalBarInitialWorldPos + verticalBar.transform.TransformVector(Vector3.right) * verticalBarOffset;
+        }
+
+        if (horizontalBar != null && rightDial != null)
+        {
+            float delta = rightDial.Angle - previousRightDialAngle;
+            previousRightDialAngle = rightDial.Angle;
+            horizontalBarOffset = Mathf.Clamp(horizontalBarOffset + delta * horizontalBarSensitivity, horizontalBarOffsetClamp.x, horizontalBarOffsetClamp.y);
+            horizontalBar.transform.position = horizontalBarInitialWorldPos + horizontalBar.transform.TransformVector(Vector3.up) * horizontalBarOffset;
+        }
+
+        if (horizontalBar != null && verticalBar != null)
+        {
+            Vector3 hLocal = horizontalBar.transform.localPosition;
+            hLocal.x = verticalBar.transform.localPosition.x;
+            horizontalBar.transform.localPosition = hLocal;
+        }
+    }
+
+#if UNITY_EDITOR
+    private void OnDrawGizmos()
+    {
+        if (verticalBar != null)
+        {
+            Vector3 baseWorld = Application.isPlaying ? verticalBarInitialWorldPos : verticalBar.transform.position;
+            DrawBarRangeGizmo(verticalBar.transform, baseWorld, Vector3.right, verticalBarOffsetClamp, Color.cyan);
+        }
+        if (horizontalBar != null)
+        {
+            Vector3 baseWorld = Application.isPlaying ? horizontalBarInitialWorldPos : horizontalBar.transform.position;
+            DrawBarRangeGizmo(horizontalBar.transform, baseWorld, Vector3.up, horizontalBarOffsetClamp, Color.magenta);
+        }
+    }
+
+    private static void DrawBarRangeGizmo(Transform bar, Vector3 baseWorld, Vector3 localAxis, Vector2 clamp, Color color)
+    {
+        Vector3 axisWorld = bar.TransformVector(localAxis);
+        Vector3 minWorld = baseWorld + axisWorld * clamp.x;
+        Vector3 maxWorld = baseWorld + axisWorld * clamp.y;
+
+        Gizmos.color = color;
+        Gizmos.DrawLine(minWorld, maxWorld);
+        Gizmos.DrawWireSphere(minWorld, 0.02f);
+        Gizmos.DrawWireSphere(maxWorld, 0.02f);
+        Gizmos.DrawWireSphere(baseWorld, 0.015f);
+    }
+#endif
 }
